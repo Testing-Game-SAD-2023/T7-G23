a4f410efb16d48b163a8c05ff9bdce8ab2e75e6f
zurbojack <michele.savella99@gmail.com>
2023-06-15 20:44:57 +0200

    Release 1.1
    Added the support for GET request with paths in addition to the urls.
    
    Modified the programs' versions used by the inner project (now it uses java 20 and maven 3.8.3)

--- compExec/src/main/java/com/T07/compExec/service/Manager.java
+++ compExec/src/main/java/com/T07/compExec/service/Manager.java
@@ -14,14 +14,15 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class Manager {
 
     private final String pathMVNProject = "/projectUT/";
-    private final String pathMVNProject_class = "/projectUT/src/main/java/pacchetto/";
-    private final String pathMVNProject_testclass = "/projectUT/src/test/java/pacchetto/";
+    private final String pathMVNProject_class = "/projectUT/src/main/java/TestPackage/";
+    private final String pathMVNProject_testclass = "/projectUT/src/test/java/TestPackage/";
 
     public CompExecResults doCompExec(String pathClass, String pathTestClassth, String pathCoverage) {
 
@@ -37,18 +38,32 @@ public class Manager {
 
         try {
 
-            InputStream inClass = new URL(urlClasse).openStream();
-            String filenameClass = Paths.get(new URI(urlClasse).getPath()).getFileName().toString();
-            Files.copy(inClass, Paths.get(pathMVNProject_class+filenameClass), StandardCopyOption.REPLACE_EXISTING);
+            if (urlClasse.contains("http")) {
 
-            InputStream inTestClass = new URL(urlClassetest).openStream();
-            String filenameTestClass = Paths.get(new URI(urlClassetest).getPath()).getFileName().toString();
-            Files.copy(inTestClass, Paths.get(pathMVNProject_testclass+filenameTestClass), StandardCopyOption.REPLACE_EXISTING);
+                InputStream inClass = new URL(urlClasse).openStream();
+                String filenameClass = Paths.get(new URI(urlClasse).getPath()).getFileName().toString();
+                Files.copy(inClass, Paths.get(pathMVNProject_class+filenameClass), StandardCopyOption.REPLACE_EXISTING);
+
+                InputStream inTestClass = new URL(urlClassetest).openStream();
+                String filenameTestClass = Paths.get(new URI(urlClassetest).getPath()).getFileName().toString();
+                Files.copy(inTestClass, Paths.get(pathMVNProject_testclass+filenameTestClass), StandardCopyOption.REPLACE_EXISTING);
+
+            } else {
+
+                Path sourceClass = Paths.get(urlClasse);
+                String filenameClass = Paths.get(new URI(urlClasse).getPath()).getFileName().toString();
+                Files.copy(sourceClass, Paths.get(pathMVNProject_class+filenameClass), StandardCopyOption.REPLACE_EXISTING);
+
+                Path sourceTestClass = Paths.get(urlClassetest);
+                String filenameTestClass = Paths.get(new URI(urlClassetest).getPath()).getFileName().toString();
+                Files.copy(sourceTestClass, Paths.get(pathMVNProject_testclass+filenameTestClass), StandardCopyOption.REPLACE_EXISTING);
+
+            }
 
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
-        } catch (IOException e1) {
-            throw new RuntimeException(e1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
